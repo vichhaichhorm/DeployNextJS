531e450cb772bfbf1e3a3b9a0745ba8996203764
chhorm.vichhai <vichhaichhorm169@gmail.com>
2024-07-25 15:42:29 +0700

    done

---
+++ src/app/(authentication)/login/_component/loginSuccessfulComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginSuccessfulComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginSuccessfulComponent
---
+++ src/app/(authentication)/login/page.jsx
@@ -0,0 +1,26 @@
+"use client"
+export default function LoginPage() {
+
+    async function handleSubmit(event) {
+        event.preventDefault(); 
+        const formData = {
+            email: event.target.email.value,
+            password: event.target.password.value
+        };
+        console.log('form login:', formData);
+    }
+
+    return (
+        <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+            <div className="mb-5">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" id="email" name='email' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input type="password" id="password" name='password' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+        </form>
+    );
+}
